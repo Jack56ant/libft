5a257762267f474ad702e1d70dc2692b37af9364
Jack56ant <155781891+Jack56ant@users.noreply.github.com>
2024-10-29 17:55:41 +0300

    Update ft_strtrim.c

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -12,32 +12,56 @@
 
 #include "libft.h"
 
-char    *ft_strtrim(char const *s1, char const *set)
-{
-    char *cpy;
+static int set_values(char c, const char *set) {
+    while (*set) {
+        if (*set == c)
+            return 1;
+        set++;
+    }
+    return 0;
+}
+
+char *ft_strtrim(char const *s1, char const *set) {
+    
+    if (!s1 || !set)
+        return NULL;
+    
+    size_t star;
+    char *ptr;
+    size_t end;
     size_t i;
-    size_t len;
-    size_t j;
 
-    i=0;
-    j=0;
-    len = ft_strlen(s1);
-
-    while (s1[i] == *set)
-    {
-        len--;
-        i++;
-    }
-
-    cpy = (char *)malloc(len+1);
-
+    end = ft_strlen(s1);
+    star = 0;
     i=0;
 
-    while (s1[i] != *set)
-    {
+    while (set_values((char)s1[star],set) && star < end )
+        star++;
     
-    }
-    
-    
+    while (set_values((char)s1[--end],set )&& star < end)
 
+    ptr = (char *)malloc(end - star +1);
+
+    if (!ptr)
+        return NULL;
+    
+    while (star <= end)
+         ptr[i++]= s1[star++];
+
+        ptr[i] = '\0';
+
+    return(ptr);
+}
+
+int main() {
+    char s1[] = "***s---Dünya----s****";
+    char set[] = "-*";
+
+    char *trimmed_str = ft_strtrim(s1, set);
+    if (trimmed_str) {
+        printf("O: '%s'\n", s1);
+        printf("K: '%s'\n", trimmed_str);
+        free(trimmed_str); 
+    }
+    return 0;
 }
